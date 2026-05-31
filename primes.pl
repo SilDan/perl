79140e2d4c931876396c45e6a9b89014ae0484f8
sildan <silvan.rincke@gmx.de>
2026-05-31 13:16:57 +0200

    Perl-2: primes.pl - learn about functions i.e. subroutines

--- primes.pl
+++ primes.pl
@@ -8,3 +8,26 @@ my $limit = 100;
 for my $i (1..$limit) {
     print "The value of 'i' is " . $i . "\n";
 }
+
+
+sayHello();
+
+# in perl, functions are called subroutines
+sub sayHello {
+    print "hello!\n";
+}
+
+# they can have parameters
+sub sayHelloTo {
+    my ($name) = @_;
+    print "hello $name!\n";
+}
+
+sayHelloTo("Strawberry");
+
+sub addNumbers{
+    my ($a,$b) = @_;
+    print $a . "+" . $b . " = " . ($a+$b) . "\n";
+}
+
+addNumbers(2,3);
